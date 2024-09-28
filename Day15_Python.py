1ebedb4c6e36fe154f015e4436a18ae52096759f
DeepakJadhav0 <deepak0jadhav0@gmail.com>
2024-09-28 18:12:21 +0530

    Day15 added

---
+++ Day15_Python.py
@@ -0,0 +1,38 @@
+# --------------------------------- Types of Error 
+# error occur when our code fails to run as expected 
+
+# SyntaxError
+# error occured due to mistake in syntax 
+# print("hellow world)
+
+# NameError - Name error occurs when name is call wrong 
+# age =  20
+# print(name)
+
+# IndexError - when invalid index is requested to access 
+# numbers = [0,1,2,3,4,5]
+# print(numbers[6])
+
+# ModuleNotFoundError - when requested module is not present or missspelled
+# import maths
+
+# AttributeError -it occurs when function is not present in module
+# import math
+# math.PI
+
+# keyError - when key is not present in dictionary
+# student = {'name' : "deepak",'age' : 22, 'country':"India"}
+# print(student['name'])
+# print(student['countr'])
+
+# TypeError 
+# 33 + 'deepak'
+
+# ImportError - when the import function is not present in module 
+# from math import power
+
+# ValueError - when the value assigned is wrong 
+# int('a33')
+
+# ZeroDivisionError - when a number is divided by zero this typw of error occurs 
+33/0
