92c47957194001310f6796a6017224ee483048f1
DeepakJadhav0 <deepak0jadhav0@gmail.com>
2024-09-30 00:01:18 +0530

    Day16 completed

---
+++ Day16_date&Time.py
@@ -0,0 +1,41 @@
+# ----------------------------------- Date & Time 
+from datetime import datetime
+print(datetime.now())
+now = datetime.now()
+print(now.year)
+print(now.day)
+print(now.timestamp())
+
+from datetime import datetime
+new_year = datetime(2020, 1, 1)
+print(new_year)      # 2020-01-01 00:00:00
+day = new_year.day
+month = new_year.month
+year = new_year.year
+hour = new_year.hour
+minute = new_year.minute
+second = new_year.second
+print(day, month, year, hour, minute) #1 1 2020 0 0
+print(f'{day}/{month}/{year}, {hour}:{minute}')  # 1/1/2020, 0:0
+
+from datetime import datetime
+# current date and time
+now = datetime.now()
+t = now.strftime("%H:%M:%S")
+print("time:", t)
+time_one = now.strftime("%m/%d/%Y, %H:%M:%S")
+# mm/dd/YY H:M:S format
+print("time one:", time_one)
+time_two = now.strftime("%d/%m/%Y, %H:%M:%S")
+# dd/mm/YY H:M:S format
+print("time two:", time_two)
+
+from datetime import date
+d = date(2020, 1, 1)
+print(d)
+print('Current date:', d.today())    # 2019-12-05
+# date object of today's date
+today = date.today()
+print("Current year:", today.year)   # 2019
+print("Current month:", today.month) # 12
+print("Current day:", today.day)     # 5]
